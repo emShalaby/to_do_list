c6068aff01445c322b3a0822b80d146ee509067f
emShalaby <galaxian.dev@gmail.com>
2023-07-11 16:00:10 +0300

    add 2 factory functions that create tasks and projects

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function task(title, description, duedate, priority) {
+  return { title, description, duedate, priority };
+}
+function project(name, tasks) {
+  function addTask(task) {
+    this.tasks.push(task);
+  }
+  function deleteTask(task) {
+    this.tasks.splice(this.tasks.indexOf(task), 1);
+  }
+  return { name, tasks, addTask, deleteTask };
+}
+window.task = task;
+window.project = project;
