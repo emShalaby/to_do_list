bbbb595034482e76614edd11eac717fe534e16ee
emShalaby <galaxian.dev@gmail.com>
2023-07-11 17:38:30 +0300

    add a title and menu and view divs to main element

--- src/interface.js
+++ src/interface.js
@@ -1,14 +1,21 @@
-export  function pageLoad() {
+export function pageLoad() {
   const header = document.createElement("div");
   const main = document.createElement("div");
   const footer = document.createElement("div");
   const content = document.querySelector("#content");
+  const h1 = document.createElement("h1");
+  const menu = document.createElement("div");
+  const view = document.createElement("div");
 
   header.id = "header";
   main.id = "main";
   footer.id = "footer";
+  h1.textContent = "TO DO LIST";
 
   content.appendChild(header);
   content.appendChild(main);
   content.appendChild(footer);
+  header.appendChild(h1);
+  main.appendChild(menu);
+  main.appendChild(view);
 }
