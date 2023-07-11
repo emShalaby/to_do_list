bf451ee046541d9e9f1249c9d9bf65bdccfeda43
emShalaby <galaxian.dev@gmail.com>
2023-07-11 18:12:17 +0300

    seperate the pageLoad function into 3 different functions each function loads part of the website one for header, one for main and one for footer

--- src/interface.js
+++ src/interface.js
@@ -1,23 +1,33 @@
 export function pageLoad() {
-  const header = document.createElement("div");
-  const main = document.createElement("div");
-  const footer = document.createElement("div");
-  const content = document.querySelector("#content");
-  const h1 = document.createElement("h1");
-  const menu = document.createElement("div");
-  const view = document.createElement("div");
-  //menuProjects and menuTasks for how they appear on the menu div
-  const menuProjects = document.createElement("div");
-  const menuTasks = document.createElement("div");
-  // viewProjects and viewtasks for how they appear on the view div
-  const viewProjects = document.createElement("div");
-  const viewTasks = document.createElement("div");
+  headerLoad();
+  mainLoad();
+  footerLoad();
+}
 
-  header.id = "header";
-  main.id = "main";
-  footer.id = "footer";
+function headerLoad() {
+  const header = document.createElement("div");
+  const h1 = document.createElement("h1");
+  const content = document.querySelector("#content");
+
   h1.textContent = "TO DO LIST";
   h1.id = "website-title";
+  header.id = "header";
+  
+  content.appendChild(header);
+  header.appendChild(h1);
+}
+
+function mainLoad() {
+  const main = document.createElement("div");
+  const menu = document.createElement("div");
+  const view = document.createElement("div");
+  const menuProjects = document.createElement("div");
+  const menuTasks = document.createElement("div");
+  const viewProjects = document.createElement("div");
+  const viewTasks = document.createElement("div");
+  const content = document.querySelector("#content");
+
+  main.id = "main";
   menu.id = "menu";
   view.id = "view";
   menuProjects.id = "menu-projects";
@@ -25,13 +35,16 @@ export function pageLoad() {
   viewProjects.id = "view-projects";
   viewTasks.id = "view-tasks";
 
-  content.appendChild(header);
   content.appendChild(main);
-  content.appendChild(footer);
-  header.appendChild(h1);
   main.appendChild(menu);
   main.appendChild(view);
   menu.appendChild(menuProjects);
   menuProjects.appendChild(menuTasks);
 }
-export function addProject(project) {}
+
+function footerLoad() {
+  const footer = document.createElement("div");
+  const content = document.querySelector("#content");
+  footer.id = "footer";
+  content.appendChild(footer);
+}
