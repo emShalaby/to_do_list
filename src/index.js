35175dfcd5f665fe21327505b12af5fda63306ed
emShalaby <galaxian.dev@gmail.com>
2023-07-11 17:20:37 +0300

    move task and project funcs to their own modules

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-function task(title, description, duedate, priority) {
-  return { title, description, duedate, priority };
-}
-function project(name, tasks) {
-  function addTask(task) {
-    this.tasks.push(task);
-  }
-  function deleteTask(task) {
-    this.tasks.splice(this.tasks.indexOf(task), 1);
-  }
-  return { name, tasks, addTask, deleteTask };
-}
+
 
 
