cd2c4f2041becc3b6f51924a15e211bc05332c84
emShalaby <galaxian.dev@gmail.com>
2023-07-12 16:12:31 +0300

    add a getProjects function to storage module

--- src/storage.js
+++ src/storage.js
@@ -1,3 +1,4 @@
+import { projectGenerate } from "./project";
 export function storeProjects(project) {
   const projects = Array.from(
     JSON.parse(localStorage.getItem("projects")) || []
@@ -9,11 +10,20 @@ export function deleteProjects(projectName) {
   const projects = Array.from(
     JSON.parse(localStorage.getItem("projects")) || []
   );
-  projects.forEach((proj)=> {if(proj.name=projectName){
-    projects.splice(projects.indexOf(proj), 1);
-  }}
-  )
+  projects.forEach((proj) => {
+    if ((proj.name = projectName)) {
+      projects.splice(projects.indexOf(proj), 1);
+    }
+  });
   localStorage.setItem("projects", JSON.stringify(projects));
 }
 
+export function getProjects() {
+  const projects =
+    Array.from(JSON.parse(localStorage.getItem("projects"))) || [];
 
+  projects.forEach((project) => {
+    projects.splice(projects.indexOf(project), 1);
+    projects.push(projectGenerate(project.name, project.tasks));
+  });
+}
